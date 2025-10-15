90c2070cd3214979e585541de8d2f3f5dfa88a0d
Руслан Костылев <stoneevil@bk.ru>
2025-10-16 00:57:13 +1100

    Initial commit

---
+++ src/masks.py
@@ -0,0 +1,26 @@
+def get_mask_card_number(card_number: str) -> str:
+    """
+    Маскирует номер карты, оставляя первые 6 и последние 4 цифры видимыми.
+    """
+    card = card_number.strip()
+
+    if len(card) != 16 or not card.isdigit():
+        raise ValueError("Номер карты должен содержать ровно 16 цифр")
+
+    masked_part_card = "*" * (len(card) - 10)
+    result = card[:6] + masked_part_card + card[-4:]
+    return result
+
+
+def get_mask_account(account_number: str) -> str:
+    """
+    Маскирует номер карты, оставляя последние 4 цифры и "**" перед ними.
+    """
+    account = account_number.strip()
+    masked_account_number = "**" + account[-4:]
+    return masked_account_number
+
+
+number = input("Please enter a number: ")
+print(get_mask_card_number(number))
+print(get_mask_account(number))
