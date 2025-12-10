f8ed1cc1a2afc5fba5cb5f4233ae946e7b17709d
Руслан Костылев <stoneevil@bk.ru>
2025-12-11 00:02:35 +1100

    Code fixed

--- src/widget.py
+++ src/widget.py
@@ -12,10 +12,13 @@ def get_mask_number(number: str) -> str:
     else:
         return f"{type} {get_mask_card_number(number)}"
 
+def get_date(date: str) -> str:
+    return f"{date[8:10]}.{date[5:7]}.{date[:4]}"
 
 number = input("Please enter the account type and number: ")
 print(get_mask_number(number))
 
+print(get_date("2024-03-11T02:26:18.671407"))
 # test_1 = "Visa Platinum 7000792289606361"
 # test_2 = "Счет 73654108430135874305"
 # print(get_mask_number(test_1), get_mask_number(test_2))
